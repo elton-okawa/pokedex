7fe35740f8b13836b283ed78c4375d06e37df86c
Elton Okawa <elton.okawa@gmail.com>
2024-07-07 06:59:56 -0300

    chore: config http client

--- app/src/app/pokedex-app.config.ts
+++ app/src/app/pokedex-app.config.ts
@@ -2,10 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './pokedex-app.routes';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { baseUrlInterceptor } from './base-url.interceptor';
 
 export const pokedexAppConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor])),
   ],
 };
