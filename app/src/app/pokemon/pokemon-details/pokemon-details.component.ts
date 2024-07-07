a5a93b2bf85992cd4169e38a30309f793b8fb99e
Elton Okawa <elton.okawa@gmail.com>
2024-07-07 15:38:32 -0300

    feat: pokemon detail

--- app/src/app/pokemon/pokemon-details/pokemon-details.component.ts
+++ app/src/app/pokemon/pokemon-details/pokemon-details.component.ts
@@ -5,21 +5,60 @@ import { PokemonDetails } from '../pokemon';
 import { Observable } from 'rxjs';
 import { HttpResponse } from '../../http-response';
 import { CommonModule } from '@angular/common';
+import { toCapitalCase } from '../../string.helper';
+import { PokemonTypeComponent } from '../pokemon-type/pokemon-type.component';
 
 @Component({
   selector: 'app-pokemon-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PokemonTypeComponent],
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.scss',
 })
 export class PokemonDetailsComponent {
   route = inject(ActivatedRoute);
   pokemonService = inject(PokemonService);
-  pokemon$: Observable<HttpResponse<PokemonDetails>>;
+  result: HttpResponse<PokemonDetails> = {
+    data: null,
+    error: null,
+    loading: false,
+  };
 
   constructor() {
     const pokemonId = Number(this.route.snapshot.params['id']);
-    this.pokemon$ = this.pokemonService.getPokemonById(pokemonId);
+    this.pokemonService
+      .getPokemonById(pokemonId)
+      .subscribe((res) => (this.result = res));
+  }
+
+  get paddedNumber() {
+    return this.result.data?.id.toString().padStart(4, '0');
+  }
+
+  get capitalizedName() {
+    const pokemon = this.result.data;
+    if (!pokemon) {
+      return '';
+    }
+
+    return toCapitalCase(pokemon.name);
+  }
+
+  get weight() {
+    const pokemon = this.result.data;
+    if (!pokemon) return '';
+
+    return `${(pokemon.weight / 10).toFixed(2)} kg`;
+  }
+
+  get height() {
+    const pokemon = this.result.data;
+    if (!pokemon) return '';
+
+    return `${(pokemon.height / 10).toFixed(2)} m`;
+  }
+
+  formatStatus(status: string) {
+    return status.split('-').map(toCapitalCase).join(' ');
   }
 }
